48d3f601eb7ca8bfb8f2aa139134fe51fcc9d17f
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2025-02-01 23:55:38 -0600

    TODO: study vectorized code

--- slot-machine.R
+++ slot-machine.R
@@ -34,7 +34,7 @@ better_score <- function(symbols) {
     } else if (cherries > 0) {
         # diamonds count as cherries
         # so long as there is one real cherry
-        prize <- c(0, 2, 5)[cherries + diamonds + 1]
+        prize <- c(0, 2, 5)[cherries + diamonds + 1] # add one for proper indexing
     } else {
         prize <- 0
     }
@@ -96,3 +96,17 @@ for (i in 1:nrow(combos)) {
 head(combos, 10)
 
 sum(combos$prob * combos$prize)
+
+
+get_many_symbols <- function(n) {
+    wheel <- c("DD", "7", "BBB", "BB", "B", "C", "0")
+    vec <- sample(wheel, size = 3 * n, replace = TRUE,
+                  prob = c(0.03, 0.03, 0.06, 0.1, 0.25, 0.01, 0.52))
+    matrix(vec, ncol = 3) # Reshapes into an n x 3 matrix
+}
+
+play_many <- function(n) {
+    symb_mat <- get_many_symbols(n = n)
+    data.frame(w1 = symb_mat[,1], w2 = symb_mat[,2],
+               w3 = symb_mat[,3], prize = score_many(symb_mat))
+}
